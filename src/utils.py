b107b656ba2c213292d14c1841d6a32a2958ccc2
reezuleanu <>
2024-09-01 23:23:27 +0300

    added AI upscaling

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException
 from PIL import Image, ImageFilter
 from PIL.ImageFile import ImageFile
+from super_image import EdsrModel, ImageLoader
+from torchvision.transforms.functional import to_pil_image
 
 
 def is_image(extension: str) -> None:
@@ -14,6 +16,28 @@ def is_image(extension: str) -> None:
 
     if extension not in accepted_formats:
         raise HTTPException(400, "This file format is not accepted")
+
+
+# ! ONLY SUPPORTS x2
+def upscale(image: ImageFile, factor: int) -> ImageFile:
+    """Upscale an image with AI by a factor of 2, 3 or 4
+
+    Args:
+        image (ImageFile): image to upscale
+        factor (int): upscale factor
+
+    Returns:
+        ImageFile: upscaled image
+    """
+
+    model = EdsrModel.from_pretrained("eugenesiow/edsr-base", scale=factor)
+    inputs = ImageLoader.load_image(image)
+    preds = model(inputs)
+    preds = preds.squeeze(0)
+    return to_pil_image(preds)
+
+    ImageLoader.save_image(preds, "./scaled_2x.png")
+    ImageLoader.save_compare(inputs, preds, "./scaled_2x_compare.png")
 
 
 def apply_modifications(image_path: str, json: dict) -> ImageFile:
@@ -31,7 +55,7 @@ def apply_modifications(image_path: str, json: dict) -> ImageFile:
         "width": lambda x: image.resize((x, image.height)),
         "height": lambda y: image.resize((image.width, y)),
         "rotate": lambda x: image.rotate(x),
-        # "upscale" : lambda x : image,
+        "upscale": lambda _: upscale(image, 2),
         "blur": lambda x: image.filter(ImageFilter.GaussianBlur(x)),
         "sharpen": lambda _: image.filter(ImageFilter.SHARPEN),
         "grayscale": lambda _: image.convert("L"),
